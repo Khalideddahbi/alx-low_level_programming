b2927e6a45b98fda2c8123237467b445aefd9972
Khalideddahbi <khalid-192@hotmail.com>
2023-08-10 20:09:58 -0700

    first one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main -  entry point
+ *
+ * description: printing with puts function
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
