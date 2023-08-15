1c791c3aeae1fd590ccc6a8f9f1ee0d98c4f546e
Khalideddahbi <khalid-192@hotmail.com>
2023-08-14 18:23:43 -0700

    sd and bd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sd = 'a';
+	char bd = 'A';
+
+	for (sd = 'a'; letter <= 'z'; sd++)
+		putchar(sd);
+
+	for (bd = 'A'; bd <= 'Z'; bd++)
+		putchar(bd);
+
+	putchar('\n');
+
+	return (0);
+}
