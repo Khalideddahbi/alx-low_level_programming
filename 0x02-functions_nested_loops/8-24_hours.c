5a7ce0fbc0eabd2ffa884c8ef4142319820b9187
Khalideddahbi <khalid-192@hotmail.com>
2023-08-16 17:18:17 -0700

    day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,11 +18,11 @@ void jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 					if (a >= 2 && b >= 4)
 						break;
-				_putchar(a);
-				_putchar(b);
+				_putchar(a + 48);
+				_putchar(b + 48);
 				 _putchar(58);
-				 _putchar(c);
-				 _putchar(d);
+				 _putchar(c + 48);
+				 _putchar(d + 48);
 				 _putchar('\n');
 			}
 		}
