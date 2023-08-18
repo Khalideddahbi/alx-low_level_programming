ba042b70921a857f897d47c1ac85048a753c4b11
Khalideddahbi <khalid-192@hotmail.com>
2023-08-18 15:06:21 -0700

    exept two

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry
+ *
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c >= 48; c <= 57; c++)
+	{
+		if (c == 50 || c == 52)
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}
