1a384a83c63d02dacd337a201a7dafd9db329081
Khalideddahbi <khalid-192@hotmail.com>
2023-08-17 17:22:29 -0700

    zero

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,10 +8,11 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
+
 	/**
 	*while (i < 10)
 	*{
@@ -19,6 +20,6 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
