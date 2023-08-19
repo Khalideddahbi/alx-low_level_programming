9f68a1a79fc5f5dec42ae17b56b53e5fc7ae4925
Khalideddahbi <khalid-192@hotmail.com>
2023-08-18 18:16:18 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always o
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf("fizz ");
+		}
+		else if (i % 5 == 0 && !(i % 3 == 0))
+		{
+			printf("buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
