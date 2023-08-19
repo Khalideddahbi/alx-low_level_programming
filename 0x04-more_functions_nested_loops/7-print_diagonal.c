5bfe94dd462146a002569b213ab3ce2b9b1998bf
Khalideddahbi <khalid-192@hotmail.com>
2023-08-18 17:29:54 -0700

    diago

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,22 +9,20 @@
 
 void print_diagonal(int n)
 {
-	if (n > 0)
-	{
-		int i, j;
+	int i, j;
 
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 1; j <= i; j++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
+	if (n <= 0)
+	{
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+				_putchar(32);
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
