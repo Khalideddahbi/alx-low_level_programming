e78c6ede973c2fcf404f0f7af7b79e9d2ddca817
Khalideddahbi <khalid-192@hotmail.com>
2023-08-21 18:17:25 -0700

    other

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * rev_string - entry
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - entry
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+	}
+	_putchar('\n');
+}
