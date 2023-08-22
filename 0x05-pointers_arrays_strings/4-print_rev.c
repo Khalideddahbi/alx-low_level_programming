a083275d0b8f0cc1a91f6c3ff379113ad8345981
Khalideddahbi <khalid-192@hotmail.com>
2023-08-21 18:01:02 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - entry
+ *
+ * @s: string
+ *
+ * Return: always 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
