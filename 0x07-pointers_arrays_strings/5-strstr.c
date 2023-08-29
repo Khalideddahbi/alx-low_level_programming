4bb42dbcba6089c9cf30ff49f57d6c03d82311ad
Khalideddahbi <khalid-192@hotmail.com>
2023-08-28 18:55:44 -0700

    sub

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strstr - entry
+ *
+ * @haystack: string
+ * @needle: matching
+ *
+ * Return: pointer to start
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
