48524817e905d50eb639c5f96b6ecf2a90255d08
Khalideddahbi <khalid-192@hotmail.com>
2023-08-30 18:23:06 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - entry
+ *
+ * @x: num
+ * @y: power
+ *
+ * Return: power of num
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
