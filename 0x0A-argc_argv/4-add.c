b9b5ed9d83558443876e089bc13e38a5875a5b6a
Khalideddahbi <khalid-192@hotmail.com>
2023-09-03 16:58:21 -0700

    addi

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ *
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	int s = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			s = s + atoi(argv[argc]);
+		}
+	}
+	printf("%d\n", s);
+	return (0);
+}
