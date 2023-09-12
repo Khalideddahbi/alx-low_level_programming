8f5d3bfe1fa3e394b690028e23c7aba2a1aef17c
Khalideddahbi <khalid-192@hotmail.com>
2023-09-11 17:52:48 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - entry
+ * @name: name it
+ * @age: age of
+ * @owner: whom
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
